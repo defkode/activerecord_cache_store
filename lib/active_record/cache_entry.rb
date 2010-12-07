0d4f5e1f4e79c5f76736306f3351003798e739f7
Tomasz Mazur <defkode@gmail.com>
2010-12-07 14:10:51 +0100

    tested

---
+++ lib/active_record/cache_entry.rb
@@ -0,0 +1,17 @@
+class CacheEntry < ActiveRecord::Base
+  set_table_name :cache
+  
+  validates :key,        :presence => true, :uniqueness => true
+  validates :value,      :presence => true
+  validates :created_at, :presence => true
+  
+  scope :expired, lambda { where("expires_at < ?", Time.now.to_f) }
+  
+  def expires_in
+    expires_at ? expires_at - created_at : nil
+  end
+  
+  def to_cache_entry
+    ActiveSupport::Cache::Entry.create(value, created_at, :expires_in => expires_in)
+  end
+end

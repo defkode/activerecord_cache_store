2b39a0ba9418004fb96070f8f301347dce07aa5e
Tomasz Mazur <defkode@gmail.com>
2010-12-07 10:55:27 +0100

    README

---
+++ lib/generators/cache_migration_generator.rb
@@ -0,0 +1,14 @@
+require 'rails/generators/active_record'
+
+module Rails
+  module Generators
+    class CacheMigrationGenerator < ActiveRecord::Generators::Base
+      argument :name, :type => :string, :default => ""
+      source_root File.expand_path("../templates", __FILE__)
+      
+      def create_migration_file
+        migration_template "migration.rb", "db/migrate/create_cache.rb"
+      end
+    end
+  end
+end
